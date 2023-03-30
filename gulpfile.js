5291d63ad94233c9ce4093d3a4678f63e7a8553e
Vip Abdujamilov <vip.abdujamilov@gmail.com>
2023-03-30 20:13:23 +0500

    gulpfile.js added build command

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require("gulp");
+const csso = require("gulp-csso");
 const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
+const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require("browser-sync").create();
 
 // compile sass to css
@@ -11,7 +14,6 @@ function style() {
 		.pipe(gulp.dest("./css"))
 		.pipe(browserSync.stream());
 }
-
 function watch() {
 	browserSync.init({
 		server: {
@@ -23,5 +25,28 @@ function watch() {
 	gulp.watch("./js/**/*.js").on("change", browserSync.reload);
 }
 
+// Build
+function buildHtml() {
+	return gulp.src("./**.html").pipe(gulp.dest("dist/"));
+}
+function buildCss() {
+	return gulp
+		.src("./css/main.css")
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ["defaults"],
+			})
+		)
+		.pipe(csso())
+		.pipe(gulp.dest("dist/css"));
+}
+function buildJs() {
+	return gulp.src("./js/**.js").pipe(uglify()).pipe(gulp.dest("dist/js"));
+}
+function buildAssets() {
+	return gulp.src("./assets/**/*").pipe(gulp.dest("dist/assets"));
+}
+
 exports.style = style;
 exports.watch = watch;
+exports.build = gulp.series(buildHtml, buildCss, buildJs, buildAssets);
